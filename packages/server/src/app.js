47bf1613367268f4d0e241871c1e88f66f677140
Chris Bailey <chris@ghostly.io>
2021-03-02 00:21:08 -0500

    :sparkle: feat: example router (routers/index.js) and add comments
    add get route examples

--- packages/server/src/app.js
+++ packages/server/src/app.js
@@ -7,6 +7,9 @@ const expressPinoLogger = require('express-pino-logger');
 const logger = require('./utils/logger');
 const db = require('./utils/db');
 
+const indexRouter = require('./routers/index');
+
+// create instance of app
 const app = express();
 
 // connect to mongodb
@@ -20,12 +23,12 @@ db.connect()
 
 app.set('port', process.env.PORT || 3000);
 
+// app configs
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressPinoLogger({ logger: logger }));
 
-app.get('/', (req, res) => {
-  res.json({ hello: 'world' });
-});
+// routers
+app.use('/', indexRouter); // attach index router to root path
 
 module.exports = app;
--- packages/server/src/utils/db.js
+++ packages/server/src/utils/db.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
-const connect = async () => {
-  await mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  });
+const db = {
+  connect: async () => {
+    await mongoose.connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+  },
 };
-
-module.exports = connect;
+module.exports = db;
