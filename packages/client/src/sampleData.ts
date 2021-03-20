728ec956d944f78385e2d057cc92057d05ead788
Chris Bailey <chris@ghostly.io>
2021-03-20 08:32:15 -0400

    :art: (sampleData) styling and naming fixes
    What kind of lint rules discourages I* interface naming convention

--- packages/client/src/sampleData.ts
+++ packages/client/src/sampleData.ts
@@ -1,92 +1,95 @@
-import { Review, WebApp } from "./types";
+import { Review, WebApp } from './types';
 
-interface IReturn {
-  status: string
+interface DataReturn {
+  status: string;
 }
 
-interface IAppReturn extends IReturn {
-  data: WebApp[]
+interface AppReturn extends DataReturn {
+  data: WebApp[];
 }
 
-interface IReviewReturn extends IReturn {
-  data: Review[]
+interface ReviewReturn extends DataReturn {
+  data: Review[];
 }
 
-export const sampleApps:  IAppReturn = {
-  status: "ok",
+export const sampleApps: AppReturn = {
+  status: 'ok',
   data: [
-  {
-    manifestURL: "https://sky.shiiyu.moe/manifest.webmanifest",
-      startUrl: "https://sky.shiiyu.moe/",
-      name: "SkyCrypt",
-    description: "A beautiful site for sharing your SkyBlock profile 🌹",
+    {
+      manifestURL: 'https://sky.shiiyu.moe/manifest.webmanifest',
+      startUrl: 'https://sky.shiiyu.moe/',
+      name: 'SkyCrypt',
+      description: 'A beautiful site for sharing your SkyBlock profile 🌹',
       icon: {
-        src: "https://sky.shiiyu.moe/resources/img/app-icons/maskable-192.webp",
-          purpose: "maskable",
+        src: 'https://sky.shiiyu.moe/resources/img/app-icons/maskable-192.webp',
+        purpose: 'maskable'
       },
       screenshots: [
         {
-          src: "https://sky.shiiyu.moe/resources/img/screenshot.jpg",
-            size: { height: 2160, width: 3840 },
-        },
+          src: 'https://sky.shiiyu.moe/resources/img/screenshot.jpg',
+          size: { height: 2160, width: 3840 }
+        }
       ],
-      themeColor: "#282828",
-      backgroundColor: "#282828",
+      themeColor: '#282828',
+      backgroundColor: '#282828',
       appleMobileWebAppCapable: true,
-      averageRating: 4.8,
-  },
-  {
-    manifestURL: "https://example.com/manifest.json",
-      startUrl: "https://example.com/",
-      name: "example app",
-    description: "this app is an example app",
-    icon: {
-      src: "https://www.w3.org/TR/appmanifest/images/icon-mask-windows.svg",
-        purpose: "maskable",
+      averageRating: 4.8
     },
-    appleMobileWebAppCapable: false,
-    averageRating: 2.3,
-  },
-  {
-    manifestURL: "https://proxx.app/manifest.json",
-      startUrl: "https://proxx.app/",
-      name: "PROXX",
-    description: "Help your crew navigate space by marking out the black holes using proxx, your proximity scanner.",
-    icon: {
-      src: "https://proxx.app/assets/icon-maskable-7a2eb399.png",
-        purpose: "maskable",
+    {
+      manifestURL: 'https://example.com/manifest.json',
+      startUrl: 'https://example.com/',
+      name: 'example app',
+      description: 'this app is an example app',
+      icon: {
+        src: 'https://www.w3.org/TR/appmanifest/images/icon-mask-windows.svg',
+        purpose: 'maskable'
+      },
+      appleMobileWebAppCapable: false,
+      averageRating: 2.3
     },
-    appleMobileWebAppCapable: true,
-    themeColor: "#0e103e",
-    backgroundColor: "#0e103e",
-    averageRating: 4.3,
-  },
-] 
+    {
+      manifestURL: 'https://proxx.app/manifest.json',
+      startUrl: 'https://proxx.app/',
+      name: 'PROXX',
+      description:
+        'Help your crew navigate space by marking out the black holes using proxx, your proximity scanner.',
+      icon: {
+        src: 'https://proxx.app/assets/icon-maskable-7a2eb399.png',
+        purpose: 'maskable'
+      },
+      appleMobileWebAppCapable: true,
+      themeColor: '#0e103e',
+      backgroundColor: '#0e103e',
+      averageRating: 4.3
+    }
+  ]
 };
 
-export const sampleReviews: IReviewReturn = {
-  status: "ok",
+export const sampleReviews: ReviewReturn = {
+  status: 'ok',
   data: [
-  {
-    rating: 5,
-    review: "I love this app!",
-    name: "Nate",
-    lastUpdated: 1615845522,
-  },
-  {
-    rating: 3,
-    name: "Lorem Ipsum",
-    lastUpdated: 1615840284,
-  },
-  {
-    rating: 4,
-    name: "User32",
-    lastUpdated: 1615843606,
-  },
-  {
-    rating: 1,
-    review: "I hate this app it doesn't even work. When ever I try to open it it says app crashed.",
-    name: "Eeyore",
-    lastUpdated: 1615837503,
-  }] 
+    {
+      rating: 5,
+      review: 'I love this app!',
+      name: 'Nate',
+      lastUpdated: 1615845522
+    },
+    {
+      rating: 3,
+      name: 'Lorem Ipsum',
+      lastUpdated: 1615840284
+    },
+    {
+      rating: 4,
+      name: 'User32',
+      lastUpdated: 1615843606
+    },
+    {
+      rating: 1,
+      review:
+        "I hate this app it doesn't even work. When ever I try to open it it says app crashed.",
+      name: 'Eeyore',
+      lastUpdated: 1615837503
+    }
+  ]
 };
