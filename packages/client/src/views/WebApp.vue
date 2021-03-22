07028b8f61eb99cced3a242b3e9c1573689bc262
Nate Stringham <nstringham@wccnet.edu>
2021-03-22 06:30:06 -0400

    feat(Client): :sparkles: vuetify web-app-view

--- packages/client/src/views/WebApp.vue
+++ packages/client/src/views/WebApp.vue
@@ -1,17 +1,46 @@
 <template>
-  <div>
-    <img :src="appData.icon.src" alt="" height="128" width="128" />
-    <h2>
-      <a :href="appData.startUrl" target="_blank" rel="noopener noreferrer">
-        {{ appData.name }}
-      </a>
-    </h2>
-    <p>{{ appData.description }}</p>
-    <meter min="0" max="5" :value="appData.averageRating">
-      {{ appData.averageRating }} stars
-    </meter>
-  </div>
+  <v-card flat :loading="loading">
+    <div class="d-flex flex-no-wrap">
+      <v-avatar class="ma-3" rounded="xl" size="120" color="grey">
+        <img :src="appData.icon.src" alt="" v-if="appData" />
+      </v-avatar>
+      <div>
+        <v-card-title
+          class="text-h4"
+          v-text="(appData && appData.name) || ''"
+        ></v-card-title>
+
+        <v-card-actions class="pl-4 pr-4">
+          <v-btn
+            block
+            color="primary"
+            :href="appData && appData.startUrl"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open App
+          </v-btn>
+        </v-card-actions>
+      </div>
+    </div>
+    <v-card-text>
+      <p>{{ (appData && appData.description) || "" }}</p>
+      <v-rating
+        length="5"
+        :value="(appData && appData.averageRating) || 0"
+        readonly
+        half-increments
+        dense
+      />
+    </v-card-text>
+  </v-card>
 </template>
+
+<style lang="scss" scoped>
+.v-card__title {
+  word-break: unset;
+}
+</style>
 
 <script lang="ts">
 import { WebApp } from "@/types";
@@ -22,11 +51,21 @@ export default Vue.extend({
   name: "WebApp",
 
   data: () => ({
-    appData: null as WebApp | null
+    appData: null as WebApp | null,
+    loading: true,
+    item: {
+      color: "#1F7087",
+      src: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
+      title: "Supermodel",
+      artist: "Foster the People"
+    }
   }),
 
   created: function() {
-    this.appData = sampleApps[parseInt(this.$route.params.id)];
+    setTimeout(() => {
+      this.loading = false;
+      this.appData = sampleApps[parseInt(this.$route.params.id)];
+    }, 1_000);
   }
 });
 </script>
