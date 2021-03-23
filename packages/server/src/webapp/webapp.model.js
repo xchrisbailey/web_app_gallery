6feabcaa21760c021be9cf5b2d4f83db598b27c0
Chris Bailey <chris@ghostly.io>
2021-03-23 09:06:34 -0400

    :sparkles: (webapp.model) init web app model

--- packages/server/src/webapp/webapp.model.js
+++ packages/server/src/webapp/webapp.model.js
@@ -1,6 +1,30 @@
 const mongoose = require('mongoose');
 
-const webAppSchema = new mongoose.Schema({}, { timestamps: true });
+const webAppSchema = new mongoose.Schema(
+  {
+    manifestURL: { type: String, required: true },
+    startURL: { type: String, required: true },
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    appleMobileWebCapable: { type: 'boolean', default: false },
+    themeColor: { type: String },
+    backgroundColor: { type: String },
+    icon: {
+      src: { type: String },
+      purpose: { type: String },
+    },
+    screenshot: {
+      src: { type: String, required: true },
+      size: {
+        height: { type: Number },
+        width: { type: Number },
+      },
+      label: { type: String },
+      platform: { type: String },
+    },
+  },
+  { timestamps: true },
+);
 
 const WebApp = mongoose.model('WebApp', webAppSchema);
 
