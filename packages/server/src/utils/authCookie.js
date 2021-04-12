08faa9fb34ac8062da2ff7f3954b07db33e2bbcb
Chris Bailey <chris@ghostly.io>
2021-04-12 12:56:22 -0400

    :bug: (authCookie) date format unix -> date

--- packages/server/src/utils/authCookie.js
+++ packages/server/src/utils/authCookie.js
@@ -2,7 +2,7 @@ const moment = require('moment');
 
 // stores provided JWT in httpOnly cookie
 const authCookie = (res, token) => {
-  const exp = moment().add(5, 'days').unix();
+  const exp = moment().add(5, 'days').toDate();
   res.cookie('token', token, {
     expires: new Date(exp), // set cookie expiration to 5 days
     secure: process.env.NODE_ENV === 'production' ? true : false,
