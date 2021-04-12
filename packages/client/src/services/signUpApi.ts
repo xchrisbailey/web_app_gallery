29fd2cf99e6cdbf4beb59454462508d0464e9a60
Nate Stringham <nstringham@wccnet.edu>
2021-04-12 18:22:11 -0400

    style(Client): :art: format codce with prietter

--- packages/client/src/services/signUpApi.ts
+++ packages/client/src/services/signUpApi.ts
@@ -1,31 +1,35 @@
-import { SignUp, ApiResponse, } from "@/types";
+import { SignUp, ApiResponse } from "@/types";
 
 import axiosStatic from "axios";
 
 const axios = axiosStatic.create({
-    baseURL : window.location.origin.match(/https?:\/\/[a-z0-9\-.]*/) +":3000" +"/api",
-    validateStatus: null
-})
+  baseURL: window.location.origin.match(/https?:\/\/[a-z0-9\-.]*/) + ":3000" + "/api",
+  validateStatus: null
+});
 
-export async function submitUser(userLastName: string, userFirstName: string, userEmail: string, userPassword: string): Promise<SignUp> {
-    const request = axios.post<ApiResponse<SignUp>>("/signup", {
-      data: { firstName: userFirstName,
-      lastName: userLastName,
-      email: userEmail,
-      password: userPassword} 
-    });
-    const response = await request;
-    if (response.data.status === "error") {
-      throw response.data.message;
-    }
-    return response.data.data;
-  }
+export async function submitUser(
+  userLastName: string,
+  userFirstName: string,
+  userEmail: string,
+  userPassword: string
+): Promise<SignUp> {
+  const request = axios.post<ApiResponse<SignUp>>("/signup", {
+    data: { firstName: userFirstName, lastName: userLastName, email: userEmail, password: userPassword }
+  });
 
-export async function getUsers(): Promise<SignUp>{
-  const request = axios.get<ApiResponse<SignUp>>("/me");
   const response = await request;
-  if(response.data.status === "error"){
+
+  if (response.data.status === "error") {
     throw response.data.message;
   }
-  return response.data.data
+  return response.data.data;
+}
+
+export async function getUsers(): Promise<SignUp> {
+  const request = axios.get<ApiResponse<SignUp>>("/me");
+  const response = await request;
+  if (response.data.status === "error") {
+    throw response.data.message;
+  }
+  return response.data.data;
 }
