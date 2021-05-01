11137fdeb16be2fd707918ac49c71b72f9e2c75d
Nate Stringham <nstringham@wccnet.edu>
2021-05-01 00:42:08 -0400

    test(Client): :white_check_mark: make submitapp test simulate being logged in

--- packages/client/tests/e2e/support/commands.js
+++ packages/client/tests/e2e/support/commands.js
@@ -1,3 +1,5 @@
+/// <reference types="cypress" />
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -23,3 +25,13 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("interceptLogin", () => {
+  cy.intercept(
+    {
+      method: "GET",
+      pathname: "/api/me"
+    },
+    { fixture: "api/me/luke-skywalker.json" }
+  );
+});
