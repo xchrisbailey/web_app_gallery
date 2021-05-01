20e7df8684fea77dc65302ffa0bd2d80b0145906
Nate Stringham <nstringham@wccnet.edu>
2021-05-01 17:56:20 -0400

    build(Client): :heavy_plus_sign: add vue meta

--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -4,8 +4,11 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import VueMeta from "vue-meta";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueMeta);
 
 new Vue({
   router,
